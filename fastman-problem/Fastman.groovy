7ca4c916e9fee8285f91f46f74d25bcfb9ce096e
josdem <joseluis.delacruz@gmail.com>
2017-05-09 11:01:39 -0500

    Adding fastman problem example

---
+++ fastman-problem/Fastman.groovy
@@ -0,0 +1,26 @@
+class Fastman {
+
+	def dictionary = ["fast","fat","man","woman"]
+
+	private String find(String string){
+		String result = ''
+
+		if(string?.length() > 0){
+			for(int i=0;i<string.length()-1;i++){
+				for(int j=string.length();j!=i;j--){
+					if(dictionary.contains(string.substring(i,j))){
+						result += "${string.substring(i,j)} "
+					}
+				}
+			}
+			result
+		}
+	}
+
+	static void main(String[] args){
+		Fastman fastman = new Fastman()
+		String result = fastman.find('fastman')
+		assert 'fast man' == result.trim()
+	}
+
+}
