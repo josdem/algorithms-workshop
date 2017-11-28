02b9ca67868edf57461da979e3cc778df4761c5e
josdem <joseluis.delacruz@gmail.com>
2017-11-28 14:53:00 -0600

    Getting approach with streams

--- is-pangram/PangramVerifier.java
+++ is-pangram/PangramVerifier.java
@@ -4,16 +4,9 @@ import java.lang.Character;
 
 public class PangramVerifier {
 
-	private List<Character> alphabet = Arrays.asList('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
-
 	private Boolean isPangram(String quote){
-		Boolean allInAlphabet = true;
-		for(char c : quote.toCharArray()){
-			if(!alphabet.contains(Character.toUpperCase(c))){
-				allInAlphabet = false;
-			}
-    }
-    return allInAlphabet;
+		quote.toUpperCase().chars().filter(x -> x > 64 & x < 91).forEach(System.out::println);
+		return false;
 	}
 
 
