38de1ad2a9ad5e7919b3baeb51d54411a9525afb
josdem <joseluis.delacruz@gmail.com>
2017-11-29 17:13:55 -0600

    Fixing comipation error

--- is-pangram/PangramVerifier.java
+++ is-pangram/PangramVerifier.java
@@ -1,6 +1,6 @@
 public class PangramVerifier {
 
-  private static final Integer = ASCII_ALPHABET_SUM = 2015;
+  private static final int ASCII_ALPHABET_SUM = 2015;
 
 	private Boolean isPangram(String quote){
 		Integer sum = quote.toUpperCase().chars().filter(x -> x > 64 & x < 91).distinct().sum();
