8a60dac3baede198712c830e4ef482ef7a66972d
josdem <joseluis.delacruz@gmail.com>
2017-11-29 15:36:03 -0600

    Getting Pangram problem solved

--- is-pangram/PangramVerifier.java
+++ is-pangram/PangramVerifier.java
@@ -5,15 +5,15 @@ import java.lang.Character;
 public class PangramVerifier {
 
 	private Boolean isPangram(String quote){
-		quote.toUpperCase().chars().filter(x -> x > 64 & x < 91).forEach(System.out::println);
-		return false;
+		Integer sum = quote.toUpperCase().chars().filter(x -> x > 64 & x < 91).distinct().sum();
+		return sum == 2015;
 	}
 
 
 	public static void main(String[] args){
 		String quote = "The quick brown fox jumps over the lazy dog";
 		Boolean result = new PangramVerifier().isPangram(quote);
-		System.out.println(result);
+		assert result;
 	}
 	
 }
