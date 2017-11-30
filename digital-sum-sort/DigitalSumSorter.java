5e4f1c01494d9b866771c237c8ed0fab7b306a80
josdem <joseluis.delacruz@gmail.com>
2017-11-30 15:38:23 -0600

    Getting string collection from integer collection

--- digital-sum-sort/DigitalSumSorter.java
+++ digital-sum-sort/DigitalSumSorter.java
@@ -1,15 +1,24 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class DigitalSumSorter {
 
-	private List<Integer> getMonthName(Integer month){		
+	private List<Integer> sort(List<Integer> numbers){
+		List<String> numbersAsString = new ArrayList<String>();
+		numbersAsString = numbers.stream().map(Object::toString).collect(Collectors.toList());
+		numbersAsString.forEach(System.out::println);
 		return new ArrayList<Integer>();
 	}
 
 	public static void main(String[] args){
 		List<Integer> numbers = Arrays.asList(13, 20, 7, 4);
-		List<Integer> result = new DigitalSumSorter().digitalSumSort(numbers);
+		List<Integer> result = new DigitalSumSorter().sort(numbers);
 		result.forEach(System.out::println);
 	}
 	
 }
 
 
+ 
