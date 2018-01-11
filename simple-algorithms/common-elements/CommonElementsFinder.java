d76b51f7b78f3e7a12449ccc6cf5ed00dd19b6fd
josdem <joseluis.delacruz@gmail.com>
2018-01-11 16:51:00 -0600

    Changing System.out.println by assert

--- simple-algorithms/common-elements/CommonElementsFinder.java
+++ simple-algorithms/common-elements/CommonElementsFinder.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -11,12 +12,12 @@ public class CommonElementsFinder {
   private List<Integer> find(List<Integer> first, List<Integer> second){
     return first.stream().filter(second::contains).collect(Collectors.toList());
   }
-	
+
 	public static void main(String[] args){
 		List<Integer> first = Arrays.asList(1, 2, 3, 4, 5);
 		List<Integer> second = Arrays.asList(1, 3, 5, 7, 9);
 	  List<Integer> result = new CommonElementsFinder().find(first, second);
-	  result.forEach(System.out::println);
+	  assert new ArrayList<Integer>(Arrays.asList(1,3,5)).equals(result);
 	}
 
 }
