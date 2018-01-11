fce0bae7abe249e8e2eeb5ef5b27961fe232299f
josdem <joseluis.delacruz@gmail.com>
2018-01-11 16:34:41 -0600

    Using better names

--- simple-algorithms/sum-sets/CollectionAdder.java
+++ simple-algorithms/sum-sets/CollectionAdder.java
@@ -4,29 +4,29 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 /*
-  [1,0] => 15
-  [ 5] => 5
+  [1,0] => 10
+  [  5] =>  5
 	-------
   [1,5]
 */
 
 public class CollectionAdder {
 
-  List<Integer> integerAdder(int[] a, int[] b) {
+  List<Integer> integerAdder(int[] first, int[] second) {
   	int carrier = 0;
   	List<Integer> result = new ArrayList<Integer>();
 
- 		if(a.length == 0 && b.length == 0){
+ 		if(first.length == 0 && second.length == 0){
   		return new ArrayList<Integer>();
  		}
 
- 		int j = b.length-1;
- 	 	for(int i=a.length-1; i>=0; i--){
-  		int valueA = a[i];
+ 		int j = second.length-1;
+ 	 	for(int i=first.length-1; i>=0; i--){
+  		int valueA = first[i];
   		int valueB = 0;
 
   		if(j>=0){
-  			valueB = b[j];
+  			valueB = second[j];
   		}
 
   		int parcialValue = valueA + valueB + carrier;
@@ -43,13 +43,13 @@ public class CollectionAdder {
 
  		return result;
  	}
-	
+
 
 	public static void main(String[] args){
 	  int[] first = {1,0};
 	  int[] second = {5};
 	  List<Integer> result = new CollectionAdder().integerAdder(first, second);
-	  result.forEach(System.out::println);
+	  assert new ArrayList<Integer>(Arrays.asList(1,5)).equals(result);
 	}
 
 }
