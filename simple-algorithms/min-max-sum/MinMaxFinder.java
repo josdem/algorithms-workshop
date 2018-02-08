52d884599ffe150845c30d3e07f1974be2f2c120
josdem <joseluis.delacruz@gmail.com>
2018-02-08 15:39:21 -0600

    Completing Mini-Max Sum challenge

--- simple-algorithms/min-max-sum/MinMaxFinder.java
+++ simple-algorithms/min-max-sum/MinMaxFinder.java
@@ -6,12 +6,18 @@ import java.util.TreeSet;
 public class MinMaxFinder {
 
 	private SortedSet<Integer> find(List<Integer> numbers){
-	  return new TreeSet<Integer>();
+		SortedSet<Integer> collection = new TreeSet<Integer>();
+		for(int i=0; i<numbers.size(); i++){
+			collection.add(numbers.stream().mapToInt(it -> it).sum() - numbers.get(i));
+		}
+	  return collection;
 	}
 
 	public static void main(String[] args){
 		List<Integer> numbers = Arrays.asList(1,2,3,4,5);
-		new MinMaxFinder().find(numbers);
+		SortedSet<Integer> result = new MinMaxFinder().find(numbers);
+		assert 10 == result.first();
+		assert 14 == result.last();
 	}
 	
 }
