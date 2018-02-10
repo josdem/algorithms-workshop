80415d3ec70da5d6a6a26f20436fa764e927c3fc
josdem <joseluis.delacruz@gmail.com>
2018-02-10 15:17:26 -0600

    Solution with loop and conditional

--- simple-algorithms/grading-students/StudentsGradeCalculator.java
+++ simple-algorithms/grading-students/StudentsGradeCalculator.java
@@ -5,12 +5,20 @@ import java.util.Arrays;
 public class StudentsGradeCalculator {
 
   private List<Integer> compute(List<Integer> grades){
-    return new ArrayList<Integer>();
+    List<Integer> result = new ArrayList<Integer>();
+    for(Integer grade: grades){
+      if(grade >= 38 && grade % 5 >= 3){
+        grade = grade + 5 - (grade % 5);
+      }
+      result.add(grade);
+    }
+    return result;
   }
 
   public static void main(String[] args){
     List<Integer> grades = Arrays.asList(73, 67, 38, 33);
     List<Integer> result = new StudentsGradeCalculator().compute(grades);
+    System.out.println(result);
     assert 4 == result.size();
     assert 75 == result.get(0);
     assert 67 == result.get(1);
