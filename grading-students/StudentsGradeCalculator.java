05e020809893f9697a00021c4f674fcd142be7ea
josdem <joseluis.delacruz@gmail.com>
2018-02-10 15:29:51 -0600

    Moving to the right package

--- grading-students/StudentsGradeCalculator.java
+++ grading-students/StudentsGradeCalculator.java
@@ -1,28 +1,25 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class StudentsGradeCalculator {
 
   private List<Integer> compute(List<Integer> grades){
-    List<Integer> result = new ArrayList<Integer>();
-    for(Integer grade: grades){
-      if(grade >= 38 && grade % 5 >= 3){
-        grade = grade + 5 - (grade % 5);
-      }
-      result.add(grade);
-    }
-    return result;
+    return grades
+      .stream()
+      .map(it -> (it >=38 && it % 5 >=3) ? it + 5 - it % 5 : it)
+      .collect(Collectors.toList());
   }
 
   public static void main(String[] args){
     List<Integer> grades = Arrays.asList(73, 67, 38, 33);
     List<Integer> result = new StudentsGradeCalculator().compute(grades);
-    System.out.println(result);
     assert 4 == result.size();
     assert 75 == result.get(0);
     assert 67 == result.get(1);
     assert 40 == result.get(2);
     assert 33 == result.get(3);
   }
+
 }
