c2c541f4e5c33b77770a738d585121f6462f677c
josdem <joseluis.delacruz@gmail.com>
2018-02-11 08:30:28 -0600

    Adding house width parameter

--- simple-algorithms/apple-orange/AppleOrangeScorer.java
+++ simple-algorithms/apple-orange/AppleOrangeScorer.java
@@ -1,14 +1,16 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AppleOrangeScorer {
 
-  private List<Integer> score(){
+  private List<Integer> score(List<Integer> houseWidth){
     return new ArrayList<Integer>();
   }
 
   public static void main(String[] args){
-    List<Integer> resutl = new AppleOrangeScorer().score();
+    List<Integer> houseWidth = Arrays.asList(7, 11);
+    List<Integer> result = new AppleOrangeScorer().score(houseWidth);
     assert 2 == result.size();
     assert 1 == result.get(0);
     assert 1 == result.get(1);
