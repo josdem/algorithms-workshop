ecc3bea3147de4efd6dc30e301b925b93faa9752
josdem <joseluis.delacruz@gmail.com>
2018-02-11 08:40:00 -0600

    Completing challenge parameters

--- simple-algorithms/apple-orange/AppleOrangeScorer.java
+++ simple-algorithms/apple-orange/AppleOrangeScorer.java
@@ -4,13 +4,22 @@ import java.util.Arrays;
 
 public class AppleOrangeScorer {
 
-  private List<Integer> score(List<Integer> houseWidth){
+  private List<Integer> score(
+      List<Integer> houseWidth,
+      List<Integer> treeDistances,
+      List<Integer>larryThrows,
+      List<Integer>robThrows){
     return new ArrayList<Integer>();
   }
 
   public static void main(String[] args){
     List<Integer> houseWidth = Arrays.asList(7, 11);
-    List<Integer> result = new AppleOrangeScorer().score(houseWidth);
+    List<Integer> treeDistances = Arrays.asList(5, 15);
+    List<Integer> larryThrows = Arrays.asList(-2, 2, 1);
+    List<Integer> robThrows = Arrays.asList(5, -6);
+
+    List<Integer> result = new AppleOrangeScorer().score(houseWidth, treeDistances, larryThrows, robThrows);
+
     assert 2 == result.size();
     assert 1 == result.get(0);
     assert 1 == result.get(1);
