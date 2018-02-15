67851341e390e371b854ad445917de4f44c31f6e
josdem <joseluis.delacruz@gmail.com>
2018-02-15 14:10:46 -0600

    Adding birthday chocolate challenge

---
+++ simple-algorithms/birthday-chocolate/BirthdayChocolateCalculator.java
@@ -0,0 +1,13 @@
+
+public class BirthdayChocolateCalculator {
+
+	private Integer compute(){
+		return 0;
+	}
+
+	public static void main(String[] args){
+		Integer result = new BirthdayChocolateCalculator().compute();
+		assert 2 == result;
+	}
+
+}
