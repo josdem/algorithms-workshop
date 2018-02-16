50db6ad28abf438101c63b7371210ce0c968bb9d
josdem <joseluis.delacruz@gmail.com>
2018-02-16 16:14:08 -0600

    Adding indexes to the collection

--- simple-algorithms/birthday-chocolate/BirthdayChocolateCalculator.java
+++ simple-algorithms/birthday-chocolate/BirthdayChocolateCalculator.java
@@ -1,12 +1,35 @@
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class BirthdayChocolateCalculator {
 
-	private Integer compute(){
+	private Integer compute(List<Integer> numbers, final Integer month, final Integer day) {
+ 
+    int[] indexes = {0, 1, 2, 3, 4};
+
+    List<List<Integer>> subSets = 
+      IntStream.range(0, indexes.length - 2)
+               .mapToObj(i -> numbers.subList(indexes[i], indexes[i + 2]))
+               .collect(Collectors.toList());
+ 
+    for(List<Integer> set: subSets){
+    	set.forEach(System.out::println);
+    	System.out.println("-------");
+    }
+    
 		return 0;
 	}
 
 	public static void main(String[] args){
-		Integer result = new BirthdayChocolateCalculator().compute();
+		Integer month = 2;
+		Integer day = 3;
+		List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 2);
+		Integer result = new BirthdayChocolateCalculator().compute(numbers, month, day);
 		assert 2 == result;
 	}
 
