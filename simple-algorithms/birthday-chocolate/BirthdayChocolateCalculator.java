ca4f123740e072e1825ca7f85811141b3cf76002
josdem <joseluis.delacruz@gmail.com>
2018-02-17 15:43:47 -0600

    Fixing range

--- simple-algorithms/birthday-chocolate/BirthdayChocolateCalculator.java
+++ simple-algorithms/birthday-chocolate/BirthdayChocolateCalculator.java
@@ -8,29 +8,30 @@ import java.util.stream.Stream;
 
 public class BirthdayChocolateCalculator {
 
-	private Integer compute(List<Integer> numbers, final Integer month, final Integer day) {
- 
-    int[] indexes = {0, 1, 2, 3, 4};
+  private Integer compute(List<Integer> numbers, final Integer month, final Integer day) {
 
-    List<List<Integer>> subSets = 
-      IntStream.range(0, indexes.length - 2)
-               .mapToObj(i -> numbers.subList(indexes[i], indexes[i + 2]))
-               .collect(Collectors.toList());
- 
+    List<List<Integer>> subSets =
+      IntStream.range(1, 5)
+      .mapToObj(it -> numbers.subList(it - 1, it + 1))
+      .collect(Collectors.toList());
+
     for(List<Integer> set: subSets){
-    	set.forEach(System.out::println);
-    	System.out.println("-------");
+      set.forEach(System.out::println);
+      System.out.println("-------");
     }
-    
-		return 0;
-	}
 
-	public static void main(String[] args){
-		Integer month = 2;
-		Integer day = 3;
-		List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 2);
-		Integer result = new BirthdayChocolateCalculator().compute(numbers, month, day);
-		assert 2 == result;
-	}
+    System.out.println("---");
+
+
+    return 0;
+  }
+
+  public static void main(String[] args){
+    Integer month = 2;
+    Integer day = 3;
+    List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 2);
+    Integer result = new BirthdayChocolateCalculator().compute(numbers, month, day);
+    assert 2 == result;
+  }
 
 }
