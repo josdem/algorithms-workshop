909a8375f0e7afa11609923034270d2827dcd912
josdem <joseluis.delacruz@gmail.com>
2018-03-01 14:08:43 -0600

    Adding divisible sum pairs challenge

---
+++ simple-algorithms/divisible-sum-pairs/SumPairDivider.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class SumPairDivider {
+
+	private Integer compute(List<Integer> numbers){
+		return 0;
+	}
+	
+	public static void main(String[] args){
+		List<Integer> numbers = Arrays.asList(1, 3, 2, 6, 1, 2);
+		Integer result = new SumPairDivider().compute(numbers);
+		assert 5 == result;
+	}
+
+}
