59d5c4c3d94186e901136b8329b42c167eb7a79f
josdem <joseluis.delacruz@gmail.com>
2018-03-01 16:43:49 -0600

    Solving challenge

--- simple-algorithms/bon-appetit/BonAppetitCalculator.java
+++ simple-algorithms/bon-appetit/BonAppetitCalculator.java
@@ -1,20 +1,20 @@
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class BonAppetitCalculator {
 
-	private Integer compute(List<Integer> prices, Integer itemAvoided){
+	private Integer compute(List<Integer> prices, int itemAvoided){
 		Integer charged = prices.stream().mapToInt(Integer::intValue).sum() / 2;
 		prices.remove(itemAvoided);
-		Integer fair = sharedItems.stream().mapToInt(Integer::intValue).sum() / 2;
-
+		Integer fair = prices.stream().mapToInt(Integer::intValue).sum() / 2;
 		return charged - fair;
 	}
 
 	public static void main(String[] args){
-		List<Integer> prices = Arrays.asList(3, 10, 2, 9);
-		Integer itemAvoided = 1;
+		List<Integer> prices = new LinkedList<Integer>(Arrays.asList(3, 10, 2, 9));
+		int itemAvoided = 1;
 		Integer result = new BonAppetitCalculator().compute(prices, itemAvoided);
 		assert 5 == result;
 	}
