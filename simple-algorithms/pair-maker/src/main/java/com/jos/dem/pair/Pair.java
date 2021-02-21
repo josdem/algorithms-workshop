4db6345adfef97f6c2848be6a13cc02ea653147a
josdem <joseluis.delacruz@gmail.com>
2021-02-21 15:05:05 -0500

    Deleting unused files

--- simple-algorithms/pair-maker/src/main/java/com/jos/dem/pair/Pair.java
+++ simple-algorithms/pair-maker/src/main/java/com/jos/dem/pair/Pair.java
@@ -6,13 +6,8 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Pair {
-
-    Pair(Integer a, Integer b){
-        this.a = a;
-        this.b = b;
-    }
-
     private Integer a;
     private Integer b;
 }
