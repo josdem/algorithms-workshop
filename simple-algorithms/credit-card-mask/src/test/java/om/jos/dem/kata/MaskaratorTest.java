e8829d926c5b77be0fe0896946d4fa6362f819e3
josdem <joseluis.delacruz@gmail.com>
2021-06-19 10:52:15 -0400

    Adding new credit card kata

---
+++ simple-algorithms/credit-card-mask/src/test/java/om/jos/dem/kata/MaskaratorTest.java
@@ -0,0 +1,21 @@
+package om.jos.dem.kata;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.jos.dem.kata.Maskarator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MaskaratorTest {
+
+  private Maskarator maskarator = new Maskarator();
+
+  @Test
+  @DisplayName("Maskify strings")
+  void shouldMaskifyString() {
+    assertEquals("############5616", maskarator.maskify("4556364607935616"));
+    assertEquals("#######5616", maskarator.maskify("64607935616"));
+    assertEquals("1", maskarator.maskify("1"));
+    assertEquals("", maskarator.maskify(""));
+  }
+}
