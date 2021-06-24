76b922924e9638f231bd22c1d8d22e656b7c8149
josdem <joseluis.delacruz@gmail.com>
2021-06-23 21:04:07 -0400

    Formatting the code

--- simple-algorithms/create-phone-number/src/main/java/com/jos/dem/kata/PhoneNumberGenerator.java
+++ simple-algorithms/create-phone-number/src/main/java/com/jos/dem/kata/PhoneNumberGenerator.java
@@ -2,7 +2,7 @@ package com.jos.dem.kata;
 
 public class PhoneNumberGenerator {
 
-    public String format(int[] array) {
-        return null;
-    }
+  public String format(int[] array) {
+    return null;
+  }
 }
--- simple-algorithms/create-phone-number/src/test/java/com/jos/dem/kata/PhoneNumberGeneratorTest.java
+++ simple-algorithms/create-phone-number/src/test/java/com/jos/dem/kata/PhoneNumberGeneratorTest.java
@@ -1,17 +1,18 @@
 package com.jos.dem.kata;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class PhoneNumberGeneratorTest {
 
-    private PhoneNumberGenerator phoneNumberGenerator = new PhoneNumberGenerator();
+  private PhoneNumberGenerator phoneNumberGenerator = new PhoneNumberGenerator();
 
-    @Test
-    @DisplayName("Format a phone number")
-    void shouldFormatPhoneNumber(){
-        assertEquals("(123) 456-7890", phoneNumberGenerator.format(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
-    }
+  @Test
+  @DisplayName("Format a phone number")
+  void shouldFormatPhoneNumber() {
+    assertEquals(
+        "(123) 456-7890", phoneNumberGenerator.format(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+  }
 }
