4d85773689eed2b66388fa19306f19d4507a1b8a
josdem <joseluis.delacruz@gmail.com>
2021-06-24 08:45:05 -0400

    Solving number kata

--- simple-algorithms/create-phone-number/src/main/java/com/jos/dem/kata/PhoneNumberGenerator.java
+++ simple-algorithms/create-phone-number/src/main/java/com/jos/dem/kata/PhoneNumberGenerator.java
@@ -1,8 +1,17 @@
 package com.jos.dem.kata;
 
+import java.util.Arrays;
+
 public class PhoneNumberGenerator {
 
   public String format(int[] array) {
-    return null;
+    if (array.length != 10) {
+      throw new IllegalArgumentException("Array length must be 10");
+    }
+    final StringBuilder sb = new StringBuilder();
+    String arrayAsString =
+        Arrays.stream(array).mapToObj(number -> String.valueOf(number)).reduce("", String::concat);
+    sb.append(arrayAsString).insert(0, '(').insert(4, ')').insert(5, ' ').insert(9, '-');
+    return sb.toString();
   }
 }
