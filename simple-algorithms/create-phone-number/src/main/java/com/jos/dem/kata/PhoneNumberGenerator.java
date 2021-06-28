392e0237e828b1726dcdb7d982bdaeea9a15835c
josdem <joseluis.delacruz@gmail.com>
2021-06-28 19:43:13 -0400

    Adding phone number generator instructions

--- simple-algorithms/create-phone-number/src/main/java/com/jos/dem/kata/PhoneNumberGenerator.java
+++ simple-algorithms/create-phone-number/src/main/java/com/jos/dem/kata/PhoneNumberGenerator.java
@@ -2,6 +2,10 @@ package com.jos.dem.kata;
 
 import java.util.Arrays;
 
+/*
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+ */
+
 public class PhoneNumberGenerator {
 
   public String format(int[] array) {
