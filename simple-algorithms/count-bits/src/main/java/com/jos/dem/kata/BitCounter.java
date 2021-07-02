c383f6b7b46c4a1c72394bf0e7d02f9930dd1031
josdem <joseluis.delacruz@gmail.com>
2021-07-02 10:59:31 -0400

    Adding counting bits kata solution

--- simple-algorithms/count-bits/src/main/java/com/jos/dem/kata/BitCounter.java
+++ simple-algorithms/count-bits/src/main/java/com/jos/dem/kata/BitCounter.java
@@ -2,6 +2,12 @@ package com.jos.dem.kata;
 
 public class BitCounter {
   public int countBits(int number) {
-    return 0;
+    Long result =
+        Integer.toBinaryString(number)
+            .chars()
+            .mapToObj(ch -> (char) ch)
+            .filter(ch -> ch == '1')
+            .count();
+    return result.intValue();
   }
 }
