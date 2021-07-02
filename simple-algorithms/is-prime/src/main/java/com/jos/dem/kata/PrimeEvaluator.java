70fba3cd9cbcca95cb372d7c2bcc565a5666aad8
josdem <joseluis.delacruz@gmail.com>
2021-07-01 20:36:21 -0400

    Adding is prime kata solution

--- simple-algorithms/is-prime/src/main/java/com/jos/dem/kata/PrimeEvaluator.java
+++ simple-algorithms/is-prime/src/main/java/com/jos/dem/kata/PrimeEvaluator.java
@@ -1,8 +1,13 @@
 package com.jos.dem.kata;
 
+/*
+Define a function that takes one integer argument and returns logical value true or false depending on if the integer is a prime.
+Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+ */
+
 public class PrimeEvaluator {
 
   public boolean isPrime(int number) {
-    return false;
+    return number > 1 && number % 1 == 0 && number / number == 1 && number % 5 != 0;
   }
 }
