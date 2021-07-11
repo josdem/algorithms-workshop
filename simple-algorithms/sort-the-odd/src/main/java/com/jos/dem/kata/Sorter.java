7fc727002d4b6d9f1212581cd57173170e1f36ce
josdem <joseluis.delacruz@gmail.com>
2021-07-11 19:33:14 -0400

    Refactoring sorting the odds

--- simple-algorithms/sort-the-odd/src/main/java/com/jos/dem/kata/Sorter.java
+++ simple-algorithms/sort-the-odd/src/main/java/com/jos/dem/kata/Sorter.java
@@ -4,31 +4,14 @@ package com.jos.dem.kata;
 You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
  */
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.stream.IntStream;
 
 public class Sorter {
 
   public int[] sort(int[] array) {
-    int[] result = array.clone();
-    final List<Integer> indexes = new ArrayList<>();
-    final List<Integer> values = new ArrayList<>();
-
-    IntStream.range(0, result.length)
-        .filter(element -> result[element] % 2 != 0)
-        .mapToObj(index -> index)
-        .forEach(
-            index -> {
-              indexes.add(index);
-              values.add(result[index]);
-            });
-
-    Collections.sort(indexes);
-    Collections.sort(values);
-
-    IntStream.range(0, indexes.size()).forEach(i -> result[indexes.get(i)] = values.get(i));
-    return result;
+    Iterator<Integer> odds = IntStream.of(array).filter(n -> n % 2 == 1).sorted().iterator();
+    return Arrays.stream(array).map(n -> n % 2 == 0 ? n : odds.next()).toArray();
   }
 }
