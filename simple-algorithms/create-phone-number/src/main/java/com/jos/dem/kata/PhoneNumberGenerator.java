1fa5c6288068748a64041f461e5d88b41560e33d
josdem <joseluis.delacruz@gmail.com>
2021-07-12 08:15:27 -0400

    Deleting unused code

--- simple-algorithms/create-phone-number/src/main/java/com/jos/dem/kata/PhoneNumberGenerator.java
+++ simple-algorithms/create-phone-number/src/main/java/com/jos/dem/kata/PhoneNumberGenerator.java
@@ -9,12 +9,9 @@ Write a function that accepts an array of 10 integers (between 0 and 9), that re
 public class PhoneNumberGenerator {
 
   public String format(int[] array) {
-    if (array.length != 10) {
-      throw new IllegalArgumentException("Array length must be 10");
-    }
     final StringBuilder sb = new StringBuilder();
     String arrayAsString =
-        Arrays.stream(array).mapToObj(number -> String.valueOf(number)).reduce("", String::concat);
+        Arrays.stream(array).mapToObj(n -> String.valueOf(n)).reduce("", String::concat);
     sb.append(arrayAsString).insert(0, '(').insert(4, ')').insert(5, ' ').insert(9, '-');
     return sb.toString();
   }
