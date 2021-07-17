bd73d2b603013d525b7e901c10280a1fe83eead2
josdem <joseluis.delacruz@gmail.com>
2021-07-16 20:03:04 -0400

    Adding find the odd kata

---
+++ simple-algorithms/find-the-odd/src/main/java/com/jos/dem/kata/OddFinder.java
@@ -0,0 +1,12 @@
+package com.jos.dem.kata;
+
+/*
+Given an array of integers, find the one that appears an odd number of times.
+There will always be only one integer that appears an odd number of times.
+ */
+
+public class OddFinder {
+  public static int findIt(int[] array) {
+    return 0;
+  }
+}
