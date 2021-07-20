c0b348d35a4f5414846e7c14f59b9eb7bb61f933
josdem <joseluis.delacruz@gmail.com>
2021-07-19 20:33:50 -0400

    Adding find the odd kata solution

--- simple-algorithms/find-the-odd/src/main/java/com/jos/dem/kata/OddFinder.java
+++ simple-algorithms/find-the-odd/src/main/java/com/jos/dem/kata/OddFinder.java
@@ -5,8 +5,19 @@ Given an array of integers, find the one that appears an odd number of times.
 There will always be only one integer that appears an odd number of times.
  */
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class OddFinder {
   public int findIt(int[] array) {
-    return 0;
+    return IntStream.of(array)
+        .boxed()
+        .collect(Collectors.groupingBy(it -> it, Collectors.counting()))
+        .entrySet()
+        .stream()
+        .filter(it -> it.getValue() % 2 == 1)
+        .findFirst()
+        .get()
+        .getKey();
   }
 }
