035f297f0b44978e9879cd93aa00ef097808190a
josdem <joseluis.delacruz@gmail.com>
2021-07-24 17:08:47 -0400

    Fixing bigest number kata description

--- simple-algorithms/biggest-number/src/main/java/com/jos/dem/kata/BiggestNumberFinder.java
+++ simple-algorithms/biggest-number/src/main/java/com/jos/dem/kata/BiggestNumberFinder.java
@@ -1,8 +1,8 @@
 package com.jos.dem.kata;
 
 /*
-Messages with random keywords are coming! But we just care about numbers!
-Your task is to implement a function which removes all non numeric keywords and return just the biggest number
+Messages with random data are coming! But we just care about numbers!
+Your task is to implement a function which removes all non numeric data and return just the biggest number
 messages = ["hi", "2", "@#$%", "32"]
 result = 32
 */
