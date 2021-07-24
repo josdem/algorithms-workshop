128f83907b95b13f3c7add410ada4eecb055135a
josdem <joseluis.delacruz@gmail.com>
2021-07-24 18:12:17 -0400

    Adding bon appetit kata

--- simple-algorithms/bon-appetit/BonAppetitCalculator.java
+++ simple-algorithms/bon-appetit/BonAppetitCalculator.java
@@ -1,22 +1,20 @@
-import java.util.List;
 import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class BonAppetitCalculator {
 
-	private Integer compute(List<Integer> prices, int itemAvoided){
-		Integer charged = prices.stream().mapToInt(Integer::intValue).sum() / 2;
-		prices.remove(itemAvoided);
-		Integer fair = prices.stream().mapToInt(Integer::intValue).sum() / 2;
-		return charged - fair;
-	}
+  private Integer compute(List<Integer> prices, int itemAvoided) {
+    Integer charged = prices.stream().mapToInt(Integer::intValue).sum() / 2;
+    prices.remove(itemAvoided);
+    Integer fair = prices.stream().mapToInt(Integer::intValue).sum() / 2;
+    return charged - fair;
+  }
 
-	public static void main(String[] args){
-		List<Integer> prices = new LinkedList<Integer>(Arrays.asList(3, 10, 2, 9));
-		int itemAvoided = 1;
-		Integer result = new BonAppetitCalculator().compute(prices, itemAvoided);
-		assert 5 == result;
-	}
-	
+  public static void main(String[] args) {
+    List<Integer> prices = new LinkedList<Integer>(Arrays.asList(3, 10, 2, 9));
+    int itemAvoided = 1;
+    Integer result = new BonAppetitCalculator().compute(prices, itemAvoided);
+    assert 5 == result;
+  }
 }
