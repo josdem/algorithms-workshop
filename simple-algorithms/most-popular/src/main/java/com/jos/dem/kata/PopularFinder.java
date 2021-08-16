a591f4f4ca30753a5ae09df3fdfbcbf7989bbd1a
josdem <joseluis.delacruz@gmail.com>
2021-08-15 20:07:07 -0400

    Adding most popular kata solution

--- simple-algorithms/most-popular/src/main/java/com/jos/dem/kata/PopularFinder.java
+++ simple-algorithms/most-popular/src/main/java/com/jos/dem/kata/PopularFinder.java
@@ -2,13 +2,24 @@ package com.jos.dem.kata;
 
 /*
 Given a collection with numbers: [34 , 31, 34, 56, 12, 35, 24, 34, 69, 18]
-Write a function that finds most popular number in the array, in this case “34” because it is the number that appears most often.
+Write a function that finds most popular number in the array, in this case 34 because it is the number that appears most often.
  */
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class PopularFinder {
-    public int find(List<Integer> numbers) {
-        return numbers.size();
-    }
+
+  public int find(List<Integer> numbers) {
+    return numbers.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+        .entrySet()
+        .stream()
+        .max(Comparator.comparing(Map.Entry::getValue))
+        .get()
+        .getKey();
+  }
 }
