7c83caf41ef012cc4d3c72781afca3f82aa86f15
josdem <joseluis.delacruz@gmail.com>
2021-08-21 18:06:40 -0400

    Adding first non repeated number kata brute force solution

--- simple-algorithms/first-non-repeated/src/main/java/com/jos/dem/kata/NonRepeatedFinder.java
+++ simple-algorithms/first-non-repeated/src/main/java/com/jos/dem/kata/NonRepeatedFinder.java
@@ -5,11 +5,19 @@ Given a numbers collection find fist non-repeated number in that collection
 When we have: [25, 25, 18, 18, 18, 36, 1, 2, 2, 34] first non-repeated number is 36
  */
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NonRepeatedFinder {
 
     public int find(List<Integer> numbers) {
-        return numbers.size();
+        List<Integer> unique = new ArrayList<>();
+
+        numbers.stream().forEach(n -> {
+            if(numbers.stream().filter(it -> it == n).count() == 1) {
+                unique.add(n);
+            }
+        });
+        return unique.get(0);
     }
 }
