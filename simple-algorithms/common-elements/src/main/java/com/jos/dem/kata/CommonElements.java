7dce09bd00a921e8068ebc06c17c09f4c3527508
josdem <joseluis.delacruz@gmail.com>
2021-09-08 21:54:59 -0500

    Adding common elements kata

---
+++ simple-algorithms/common-elements/src/main/java/com/jos/dem/kata/CommonElements.java
@@ -0,0 +1,17 @@
+package com.jos.dem.kata;
+
+/*
+ Given two arrays with integers.
+ Then I want to return elements in common.
+ Input: [1,2,3,4,5] and [1,3,5,7,9]
+ Output: [1, 3 ,5]
+ */
+
+import java.util.List;
+
+public class CommonElements {
+
+    public List<Integer> find(List<Integer> first, List<Integer> second) {
+        return first;
+    }
+}
