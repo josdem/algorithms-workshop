f28287179eb28834950f32c363ad8bc672ec4bb8
josdem <joseluis.delacruz@gmail.com>
2021-11-07 18:40:28 -0500

    Adding solution

--- simple-algorithms/common-elements/src/test/java/com/jos/dem/kata/CommonElementsTest.java
+++ simple-algorithms/common-elements/src/test/java/com/jos/dem/kata/CommonElementsTest.java
@@ -9,11 +9,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CommonElementsTest {
 
-    private CommonElements commonElements = new CommonElements();
+  private CommonElements commonElements = new CommonElements();
 
-    @Test
-    @DisplayName("find common elements")
-    void shouldFindCommonElements(){
-        assertEquals(Arrays.asList(1,3,5), commonElements.find(Arrays.asList(1, 2, 3, 4, 5), Arrays.asList(1, 3, 5, 7, 9)));
-    }
+  @Test
+  @DisplayName("find common elements")
+  void shouldFindCommonElements() {
+    assertEquals(
+        Arrays.asList(1, 3, 5),
+        commonElements.find(Arrays.asList(1, 2, 3, 4, 5), Arrays.asList(1, 3, 5, 7, 9)));
+  }
 }
