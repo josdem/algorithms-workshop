b32abd18de33e9cd38eba43d9680978f81fb4779
josdem <joseluis.delacruz@gmail.com>
2021-11-25 17:22:45 -0600

    Adding counting vowels function

--- simple-algorithms/counting-characters/src/main/java/com/jos/dem/kata/CharactersCounter.java
+++ simple-algorithms/counting-characters/src/main/java/com/jos/dem/kata/CharactersCounter.java
@@ -1,8 +1,19 @@
 package com.jos.dem.kata;
 
+/*
+ * Counting vowels and consonants. Create two functions one for counting vowels and another for
+ * Given: A string
+ * When: That string is josdem
+ * Then: Counting vowels should be 2 and consonants 4
+ */
+
+import java.util.Arrays;
+import java.util.List;
+
 public class CharactersCounter {
 
     public int countVowels(String keyword) {
-        return keyword.length();
+        List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u');
+        return (int) keyword.chars().filter(ch -> vowels.contains((char)ch)).count();
     }
 }
