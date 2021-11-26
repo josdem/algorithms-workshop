984b025579d0701a4858be35f4288f7fcd609510
josdem <joseluis.delacruz@gmail.com>
2021-11-26 16:07:36 -0600

    Adding counting characters solution

--- simple-algorithms/counting-characters/src/test/java/com/jos/dem/kata/CharactersCounterTest.java
+++ simple-algorithms/counting-characters/src/test/java/com/jos/dem/kata/CharactersCounterTest.java
@@ -6,15 +6,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CharactersCounterTest {
 
+  private final String nickname = "josdem";
   private CharactersCounter charactersCounter = new CharactersCounter();
 
   @Test
   void shouldCountVowels() {
-    assertEquals(2, charactersCounter.countVowels("josdem"));
+    assertEquals(2, charactersCounter.countVowels(nickname));
   }
 
   @Test
   void shouldCountConsonants() {
-    assertEquals(4, charactersCounter.countConsonants("josdem"));
+    assertEquals(4, charactersCounter.countConsonants(nickname));
   }
 }
