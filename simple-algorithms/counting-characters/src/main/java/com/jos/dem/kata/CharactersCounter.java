26ec001fcff4c1ffc9528db9fd7ed7088f2a3bb3
josdem <joseluis.delacruz@gmail.com>
2021-12-23 15:06:47 -0600

    Fixing description

--- simple-algorithms/counting-characters/src/main/java/com/jos/dem/kata/CharactersCounter.java
+++ simple-algorithms/counting-characters/src/main/java/com/jos/dem/kata/CharactersCounter.java
@@ -1,7 +1,7 @@
 package com.jos.dem.kata;
 
 /*
- * Counting vowels and consonants. Create two functions one for counting vowels and another for
+ * Counting vowels and consonants. Create two functions one for counting vowels and another for counting consonants
  * Given: A string
  * When: That string is josdem
  * Then: Counting vowels should be 2 and consonants 4
