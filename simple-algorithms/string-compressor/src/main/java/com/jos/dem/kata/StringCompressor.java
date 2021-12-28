13d145d8f54dd34cdcd3a8e9a0f2520002a4e739
josdem <joseluis.delacruz@gmail.com>
2021-12-27 19:25:43 -0600

    Adding string compressor solution

--- simple-algorithms/string-compressor/src/main/java/com/jos/dem/kata/StringCompressor.java
+++ simple-algorithms/string-compressor/src/main/java/com/jos/dem/kata/StringCompressor.java
@@ -6,9 +6,23 @@ When we call compress method
 Then we have a compressed string like "a3b4c2"
  */
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class StringCompressor {
 
   public String compress(String keyword) {
-    return keyword;
+    Map<Object, Long> map =
+        keyword
+            .chars()
+            .mapToObj(it -> (char) it)
+            .collect(Collectors.groupingBy(it -> it, Collectors.counting()));
+    StringBuilder sb = new StringBuilder();
+    map.forEach(
+        (k, v) -> {
+          sb.append(k);
+          sb.append(v);
+        });
+    return sb.toString();
   }
 }
