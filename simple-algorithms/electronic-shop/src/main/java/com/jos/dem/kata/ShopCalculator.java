a1220af7f44cf95a9b6f4dc65ac37ccb6f5282a5
josdem <joseluis.delacruz@gmail.com>
2022-01-05 20:46:09 -0600

    Adding cartesian product in a Map entry list

--- simple-algorithms/electronic-shop/src/main/java/com/jos/dem/kata/ShopCalculator.java
+++ simple-algorithms/electronic-shop/src/main/java/com/jos/dem/kata/ShopCalculator.java
@@ -1,10 +1,21 @@
 package com.jos.dem.kata;
 
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class ShopCalculator {
 
   public int compute(Integer amount, List<Integer> keyboards, List<Integer> usbs) {
+    List<Map.Entry<Integer, Integer>> pairs = new ArrayList<>();
+
+    keyboards.forEach(
+        k ->
+            usbs.forEach(
+                u -> {
+                  pairs.add(new SimpleEntry<>(k, u));
+                }));
     return amount + keyboards.size() + usbs.size();
   }
 }
