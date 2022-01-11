056a2730ee2142b11e987d5a926fd3a0680eb02a
josdem <joseluis.delacruz@gmail.com>
2022-01-11 17:28:38 -0600

    Adding square my list kata solution

--- simple-algorithms/square-calculator/src/main/java/com/jos/dem/kata/SquareCalculator.java
+++ simple-algorithms/square-calculator/src/main/java/com/jos/dem/kata/SquareCalculator.java
@@ -1,9 +1,10 @@
 package com.jos.dem.kata;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SquareCalculator {
-    public List<Integer> square(List<Integer> numbers) {
-        return numbers;
-    }
+  public List<Integer> square(List<Integer> numbers) {
+    return numbers.stream().map(it -> it * it).collect(Collectors.toList());
+  }
 }
