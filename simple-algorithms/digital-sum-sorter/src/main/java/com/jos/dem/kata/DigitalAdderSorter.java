31ac6c156be5b3d3032cefd9022fce156fa78b07
josdem <joseluis.delacruz@gmail.com>
2022-01-14 15:32:15 -0600

    Adding kata solution

--- simple-algorithms/digital-sum-sorter/src/main/java/com/jos/dem/kata/DigitalAdderSorter.java
+++ simple-algorithms/digital-sum-sorter/src/main/java/com/jos/dem/kata/DigitalAdderSorter.java
@@ -1,9 +1,14 @@
 package com.jos.dem.kata;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DigitalAdderSorter {
   public List<Integer> addAndSort(List<Integer> numbers) {
-    return numbers;
+    List<String> numbersAsString =
+        numbers.stream().map(it -> String.valueOf(it)).collect(Collectors.toList());
+    return numbersAsString.stream()
+        .map(string -> string.chars().map(ch -> Integer.valueOf(Character.toString(ch))).sum())
+        .collect(Collectors.toList());
   }
 }
