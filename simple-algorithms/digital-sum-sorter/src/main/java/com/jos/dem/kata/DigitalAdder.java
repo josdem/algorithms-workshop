c50cfd8e931a7913c7a7b1aca29ce65cb23f0cc7
josdem <joseluis.delacruz@gmail.com>
2022-01-14 19:38:00 -0600

    Fixing kata name

--- simple-algorithms/digital-sum-sorter/src/main/java/com/jos/dem/kata/DigitalAdder.java
+++ simple-algorithms/digital-sum-sorter/src/main/java/com/jos/dem/kata/DigitalAdder.java
@@ -3,8 +3,8 @@ package com.jos.dem.kata;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class DigitalAdderSorter {
-  public List<Integer> addAndSort(List<Integer> numbers) {
+public class DigitalAdder {
+  public List<Integer> add(List<Integer> numbers) {
     List<String> numbersAsString =
         numbers.stream().map(it -> String.valueOf(it)).collect(Collectors.toList());
     return numbersAsString.stream()
