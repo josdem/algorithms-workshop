30dc00a0655c52b27ca91aca27d281d8f9c2a529
josdem <joseluis.delacruz@gmail.com>
2022-01-26 17:25:03 -0600

    Adding char argument

--- simple-algorithms/counting-character/src/main/java/com/jos/dem/kata/CharacterCounter.java
+++ simple-algorithms/counting-character/src/main/java/com/jos/dem/kata/CharacterCounter.java
@@ -1,7 +1,7 @@
 package com.jos.dem.kata;
 
 public class CharacterCounter {
-  public int count(String keyword) {
-    return keyword.length();
+  public int count(String keyword, char ch) {
+    return keyword.length() + Integer.valueOf(ch);
   }
 }
