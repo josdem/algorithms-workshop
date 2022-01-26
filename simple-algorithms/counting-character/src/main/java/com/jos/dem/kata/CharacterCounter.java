004fd58925e908867f04ac8c684f22e1552a1445
josdem <joseluis.delacruz@gmail.com>
2022-01-26 17:30:54 -0600

    Adding counting character kata solution

--- simple-algorithms/counting-character/src/main/java/com/jos/dem/kata/CharacterCounter.java
+++ simple-algorithms/counting-character/src/main/java/com/jos/dem/kata/CharacterCounter.java
@@ -2,6 +2,6 @@ package com.jos.dem.kata;
 
 public class CharacterCounter {
   public int count(String keyword, char ch) {
-    return keyword.length() + Integer.valueOf(ch);
+    return (int) keyword.chars().filter(it -> ch == (char) it).count();
   }
 }
