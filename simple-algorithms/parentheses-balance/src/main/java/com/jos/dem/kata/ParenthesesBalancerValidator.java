9db22729a2477588a7fa2e8cb434c1f922413f61
josdem <joseluis.delacruz@gmail.com>
2022-02-02 13:30:49 -0500

    Adding parentheses balancer challenge solution

--- simple-algorithms/parentheses-balance/src/main/java/com/jos/dem/kata/ParenthesesBalancerValidator.java
+++ simple-algorithms/parentheses-balance/src/main/java/com/jos/dem/kata/ParenthesesBalancerValidator.java
@@ -3,6 +3,11 @@ package com.jos.dem.kata;
 public class ParenthesesBalancerValidator {
 
     public boolean isBalanced(String keyword) {
-        return false;
+        if(keyword == null) {
+            return false;
+        }
+        long opened = keyword.chars().filter(ch -> (char)ch == '(').count();
+        long closed = keyword.chars().filter(ch -> (char)ch == ')').count();
+        return opened == closed;
     }
 }
