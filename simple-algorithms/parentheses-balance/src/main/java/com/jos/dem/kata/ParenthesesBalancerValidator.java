6a0a0a12341c3e5c1953b544ceed040a042e065e
josdem <joseluis.delacruz@gmail.com>
2022-02-02 14:12:41 -0500

    Formatting code

--- simple-algorithms/parentheses-balance/src/main/java/com/jos/dem/kata/ParenthesesBalancerValidator.java
+++ simple-algorithms/parentheses-balance/src/main/java/com/jos/dem/kata/ParenthesesBalancerValidator.java
@@ -3,11 +3,11 @@ package com.jos.dem.kata;
 public class ParenthesesBalancerValidator {
 
     public boolean isBalanced(String keyword) {
-        if(keyword == null) {
+        if (keyword == null) {
             return false;
         }
-        long opened = keyword.chars().filter(ch -> (char)ch == '(').count();
-        long closed = keyword.chars().filter(ch -> (char)ch == ')').count();
+        long opened = keyword.chars().filter(ch -> (char) ch == '(').count();
+        long closed = keyword.chars().filter(ch -> (char) ch == ')').count();
         return opened == closed;
     }
 }
