d5712d62303ed8a1aa1b063a40c9e122230dedf6
josdem <joseluis.delacruz@gmail.com>
2022-03-15 17:16:37 -0400

    Adding dictionary to the main class

--- fastman-problem/fastman-java/src/main/java/com/jos/dem/challenge/FastManFinder.java
+++ fastman-problem/fastman-java/src/main/java/com/jos/dem/challenge/FastManFinder.java
@@ -2,7 +2,9 @@ package com.jos.dem.challenge;
 
 public class FastManFinder {
 
+    private final String[] dictionary = new String[]{"fast","fat","man","strong"};
+
     public String find(String string) {
-        return string;
+        return string + dictionary;
     }
 }
