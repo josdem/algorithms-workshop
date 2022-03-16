d0642c479c23a3e3757624e413de79c4cbaa86cb
josdem <joseluis.delacruz@gmail.com>
2022-03-15 21:53:59 -0400

    Debugging fastman

--- fastman-problem/fastman-java/src/main/java/com/jos/dem/challenge/FastManFinder.java
+++ fastman-problem/fastman-java/src/main/java/com/jos/dem/challenge/FastManFinder.java
@@ -1,10 +1,25 @@
 package com.jos.dem.challenge;
 
+import java.util.stream.IntStream;
+
 public class FastManFinder {
 
-    private final String[] dictionary = new String[]{"fast","fat","man","strong"};
+    private final String[] dictionary = new String[]{"fast", "fat", "man", "strong"};
+    private final StringBuilder result = new StringBuilder();
 
     public String find(String string) {
-        return string + dictionary;
+        StringBuilder sb = new StringBuilder(string);
+        IntStream.range(0, sb.length()).forEach(index -> {
+            System.out.println(sb.substring(index));
+            System.out.println("-----");
+            reverseFind(string);
+            System.out.println("-----");
+        });
+        return result.toString();
+    }
+
+    private void reverseFind(String string) {
+        StringBuilder sb = new StringBuilder(string);
+        IntStream.range(0, string.length()).map(it -> 0 - it + string.length() - 1).forEach(it -> System.out.println(sb.substring(it, string.length())));
     }
 }
