4b070cc322aacc039a52ee830302cd8818832d01
josdem <joseluis.delacruz@gmail.com>
2022-04-14 16:38:17 -0400

    Adding solution

--- fastman-challenge/fastman-groovy/src/main/groovy/com/josdem/challenge/FastManFinder.groovy
+++ fastman-challenge/fastman-groovy/src/main/groovy/com/josdem/challenge/FastManFinder.groovy
@@ -5,6 +5,14 @@ class FastManFinder {
     def dictionary = ['fast', 'fat', 'man', 'strong']
 
     def find(String keyword) {
-        return dictionary << keyword
+        def result = []
+        for(int i=0;i<keyword.length()-1;i++){
+            for(int j=keyword.length();j!=i;j--){
+                if(dictionary.contains(keyword.substring(i,j))){
+                    result << "${keyword.substring(i,j)}"
+                }
+            }
+        }
+        result
     }
 }
