85001a19ae1c0bfb59e2d950aa4a3a177ff91ef4
josdem <joseluis.delacruz@gmail.com>
2022-04-19 20:31:24 -0400

    Adding fastman kotlin solution

--- fastman-challenge/fastman-kotlin/src/main/kotlin/com/josdem/challenge/FastManFinder.kt
+++ fastman-challenge/fastman-kotlin/src/main/kotlin/com/josdem/challenge/FastManFinder.kt
@@ -1,8 +1,34 @@
 package com.josdem.challenge
 
+import java.util.stream.IntStream
+
 class FastManFinder {
+    private val dictionary = arrayOf("fast", "fat", "man", "strong")
+    private val result: MutableList<String> = ArrayList()
+    private var string: String? = null
+
     fun find(keyword: String): String {
-        return keyword
+        this.string = keyword
+        IntStream.range(0, string!!.length).forEach { index: Int -> evaluateWord(0, index) }
+        reverseFind()
+        print(result)
+        return result.joinToString(" ")
+    }
+
+    private fun reverseFind() {
+        IntStream.range(0, string!!.length).map { 0 - it + string!!.length - 1 }.forEach { index: Int ->
+            evaluateWord(
+                index,
+                string!!.length
+            )
+        }
+    }
+
+    private fun evaluateWord(from: Int, to: Int) {
+        val word = string!!.substring(from, to)
+        if (dictionary.contains(word)) {
+            result.add(word)
+        }
     }
 
 }
