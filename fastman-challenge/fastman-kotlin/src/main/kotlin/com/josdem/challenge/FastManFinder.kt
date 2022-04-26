69a3a28ad6acfd76da73f92cff9ca1a24b73d0f8
josdem <joseluis.delacruz@gmail.com>
2022-04-26 16:01:35 -0400

    Adding inclusive range

--- fastman-challenge/fastman-kotlin/src/main/kotlin/com/josdem/challenge/FastManFinder.kt
+++ fastman-challenge/fastman-kotlin/src/main/kotlin/com/josdem/challenge/FastManFinder.kt
@@ -1,7 +1,5 @@
 package com.josdem.challenge
 
-import java.util.stream.IntStream
-
 class FastManFinder {
     private val dictionary = arrayOf("fast", "fat", "man", "strong")
     private val result: MutableList<String> = ArrayList()
@@ -9,13 +7,13 @@ class FastManFinder {
 
     fun find(keyword: String): String {
         this.string = keyword
-        IntStream.range(0, string.length).forEach { index: Int -> evaluateWord(0, index) }
+        (0..string.length).forEach { index: Int -> evaluateWord(0, index) }
         reverseFind()
         return result.joinToString(" ")
     }
 
     private fun reverseFind() {
-        IntStream.range(0, string.length).map { 0 - it + string.length - 1 }.forEach { index: Int ->
+        (0..string.length).map { 0 - it + string.length }.forEach { index: Int ->
             evaluateWord(
                 index,
                 string.length
