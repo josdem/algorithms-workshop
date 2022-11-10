07f2899343c6834a0a9ba71b0ca1b1436a137de3
josdem <joseluis.delacruz@gmail.com>
2022-11-10 10:53:06 -0500

    Adding solution

--- simple-algorithms/birthday-cake-candles/src/main/java/com/jos/dem/kata/CandleCounter.java
+++ simple-algorithms/birthday-cake-candles/src/main/java/com/jos/dem/kata/CandleCounter.java
@@ -1,10 +1,13 @@
 package com.jos.dem.kata;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CandleCounter {
 
     public List<Integer> count(List<Integer> candles) {
-        return candles;
+        Optional<Integer> biggest = candles.stream().max(Integer::compare);
+        return candles.stream().filter(it -> it == biggest.get()).collect(Collectors.toList());
     }
 }
