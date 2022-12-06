02566c0837a84412b5668dda37c0d96f806502f7
josdem <jose.morales@dominos.com>
2022-12-06 11:22:15 -0500

    Deleting solution

--- simple-algorithms/array-diff/src/main/java/com/jos/dem/kata/Differentiator.java
+++ simple-algorithms/array-diff/src/main/java/com/jos/dem/kata/Differentiator.java
@@ -1,18 +1,13 @@
 package com.jos.dem.kata;
 
-/*
-Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
-It should remove all values from list a, which are present in list b keeping their order.
- */
-
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Differentiator {
 
   public int[] diff(int[] a, int[] b) {
-    List<Integer> basList = Arrays.stream(b).boxed().collect(Collectors.toList());
-    return Arrays.stream(a).filter(number -> !basList.contains(number)).toArray();
+    return a;
   }
 }
