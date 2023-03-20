f0f5df7fe0b7591ed1eae4db7ed9d1554749bdb3
josdem <joseluis.delacruz@gmail.com>
2023-03-20 10:49:21 -0400

    Formatting code

--- simple-algorithms/array-diff/src/main/java/com/jos/dem/kata/Differentiator.java
+++ simple-algorithms/array-diff/src/main/java/com/jos/dem/kata/Differentiator.java
@@ -1,13 +1,8 @@
 package com.jos.dem.kata;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Differentiator {
 
-  public int[] diff(int[] a, int[] b) {
-    return a;
-  }
+    public int[] diff(int[] a, int[] b) {
+        return a;
+    }
 }
