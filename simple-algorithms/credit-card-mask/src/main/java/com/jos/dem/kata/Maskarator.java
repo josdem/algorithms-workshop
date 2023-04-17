9a9e0771a8e2ff87caf0eafc72cd6fa9ace5f099
josdem <joseluis.delacruz@gmail.com>
2023-04-17 10:23:42 -0400

    Adding solution

--- simple-algorithms/credit-card-mask/src/main/java/com/jos/dem/kata/Maskarator.java
+++ simple-algorithms/credit-card-mask/src/main/java/com/jos/dem/kata/Maskarator.java
@@ -3,6 +3,9 @@ package com.jos.dem.kata;
 public class Maskarator {
 
   public String maskify(String keyword) {
-    return keyword;
+    return keyword.length() <= SUFFIX_LENGTH
+    ? keyword
+    : keyword.substring(0, keyword.length() - SUFFIX_LENGTH).replaceAll(".", "#")
+        + keyword.substring(keyword.length() - SUFFIX_LENGTH);
   }
 }
