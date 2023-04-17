72696cd2aca73b8c8e4ec57c627fd90a6a283b7d
josdem <joseluis.delacruz@gmail.com>
2023-04-17 10:25:04 -0400

    Adding missing constant

--- simple-algorithms/credit-card-mask/src/main/java/com/jos/dem/kata/Maskarator.java
+++ simple-algorithms/credit-card-mask/src/main/java/com/jos/dem/kata/Maskarator.java
@@ -2,6 +2,8 @@ package com.jos.dem.kata;
 
 public class Maskarator {
 
+  private static final int SUFFIX_LENGTH = 4;
+
   public String maskify(String keyword) {
     return keyword.length() <= SUFFIX_LENGTH
     ? keyword
