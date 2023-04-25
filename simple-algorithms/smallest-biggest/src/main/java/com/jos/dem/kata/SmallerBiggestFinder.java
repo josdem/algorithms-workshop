b3de90e52ee63c66c3aab5894dc5374215aa56c1
josdem <joseluis.delacruz@gmail.com>
2023-04-24 20:22:06 -0400

    Adding kata solution

--- simple-algorithms/smallest-biggest/src/main/java/com/jos/dem/kata/SmallerBiggestFinder.java
+++ simple-algorithms/smallest-biggest/src/main/java/com/jos/dem/kata/SmallerBiggestFinder.java
@@ -1,9 +1,12 @@
 package com.jos.dem.kata;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class SmallerBiggestFinder {
     public List<Integer> find(List<Integer> numbers) {
-        return numbers;
+        Integer smaller = numbers.stream().reduce((a, b) -> a < b ? a : b).get();
+        Integer biggest = numbers.stream().reduce((a, b) -> a > b ? a : b).get();
+        return Arrays.asList(smaller, biggest);
     }
 }
