8a23b4926ca86a5cbad3056ada4125e6f71280fe
josdem <joseluis.delacruz@gmail.com>
2023-05-11 08:30:09 -0400

    Adding new twice as old kata

---
+++ simple-algorithms/twice-as-old/src/test/java/com/josdem/kata/TwiceAsOldTest.java
@@ -0,0 +1,25 @@
+package com.josdem.kata;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TwiceAsOldTest {
+
+  private TwiceAsOld twiceAsOld = new TwiceAsOld();
+
+  private static final Logger log = Logger.getLogger(TwiceAsOldTest.class.getName());
+
+  @Test
+  @DisplayName("validate twice as old")
+  void shouldValidateTwiceAsOld(TestInfo testInfo) {
+    log.info("Running: " + testInfo.getDisplayName());
+    assertEquals(30, twiceAsOld.compute(30, 0));
+    assertEquals(16, twiceAsOld.compute(30, 7));
+    assertEquals(15, twiceAsOld.compute(45, 30));
+  }
+}
