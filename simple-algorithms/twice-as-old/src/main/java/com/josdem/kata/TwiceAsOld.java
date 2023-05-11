f7413c1dedfdf8185aa2cc7b812591c019a0f05d
josdem <joseluis.delacruz@gmail.com>
2023-05-11 08:41:07 -0400

    Adding kata solution

--- simple-algorithms/twice-as-old/src/main/java/com/josdem/kata/TwiceAsOld.java
+++ simple-algorithms/twice-as-old/src/main/java/com/josdem/kata/TwiceAsOld.java
@@ -2,6 +2,6 @@ package com.josdem.kata;
 
 public class TwiceAsOld {
   public int compute(int dadYears, int sonYears) {
-    return dadYears + sonYears;
+    return sonYears == 0 ? dadYears : Math.abs(dadYears - sonYears * 2);
   }
 }
