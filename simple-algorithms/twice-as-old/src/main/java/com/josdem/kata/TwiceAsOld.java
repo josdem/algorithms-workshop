24096f8d50c2c0a784782abfb215d806998fb9ac
josdem <joseluis.delacruz@gmail.com>
2023-05-11 08:43:01 -0400

    Delting unused code

--- simple-algorithms/twice-as-old/src/main/java/com/josdem/kata/TwiceAsOld.java
+++ simple-algorithms/twice-as-old/src/main/java/com/josdem/kata/TwiceAsOld.java
@@ -2,6 +2,6 @@ package com.josdem.kata;
 
 public class TwiceAsOld {
   public int compute(int dadYears, int sonYears) {
-    return sonYears == 0 ? dadYears : Math.abs(dadYears - sonYears * 2);
+    return Math.abs(dadYears - sonYears * 2);
   }
 }
