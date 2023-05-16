c963537975df0c22b64842f1a05e85155211d65a
josdem <joseluis.delacruz@gmail.com>
2023-05-15 21:40:42 -0400

    Adding kata solution

--- simple-algorithms/longest-substring/src/main/java/com/josdem/kata/LongestSubstring.java
+++ simple-algorithms/longest-substring/src/main/java/com/josdem/kata/LongestSubstring.java
@@ -1,7 +1,18 @@
 package com.josdem.kata;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+
 public class LongestSubstring {
   public int compute(String data) {
-    return data.length();
+    Set<Character> substrings = new HashSet<>();
+    char[] chars = data.toCharArray();
+    IntStream.range(0, data.toCharArray().length - 1)
+        .forEach(
+            index -> {
+              substrings.add(chars[index]);
+            });
+    return substrings.size() > 0 ? substrings.size() : 1;
   }
 }
