621136fb5bd2813d4aed103231b6129cb09014f6
josdem <joseluis.delacruz@gmail.com>
2023-05-17 09:09:51 -0400

    Refactoging solution

--- simple-algorithms/longest-substring/src/main/java/com/josdem/kata/LongestSubstring.java
+++ simple-algorithms/longest-substring/src/main/java/com/josdem/kata/LongestSubstring.java
@@ -1,18 +1,13 @@
 package com.josdem.kata;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LongestSubstring {
   public int reduce(String data) {
-    Set<Character> substrings = new HashSet<>();
-    char[] chars = data.toCharArray();
-    IntStream.range(0, data.toCharArray().length - 1)
-        .forEach(
-            index -> {
-              substrings.add(chars[index]);
-            });
+    List<String> substrings =
+        Arrays.asList(data.split("")).stream().distinct().collect(Collectors.toList());
     return substrings.size() > 0 ? substrings.size() : 1;
   }
 }
