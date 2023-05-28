9c72d67b714a03134ee13dbb02b675dc010bc845
josdem <joseluis.delacruz@gmail.com>
2023-05-28 14:04:50 -0400

    Deleting solution

--- simple-algorithms/hero-vs-dragons/src/main/java/com/josdem/kata/HeroVsDragons.java
+++ simple-algorithms/hero-vs-dragons/src/main/java/com/josdem/kata/HeroVsDragons.java
@@ -2,6 +2,6 @@ package com.josdem.kata;
 
 public class HeroVsDragons {
     public boolean fight(int bullets, int dragons){
-        return bullets / 2 >= dragons;
+        return bullets + dragons == 0;
     }
 }
