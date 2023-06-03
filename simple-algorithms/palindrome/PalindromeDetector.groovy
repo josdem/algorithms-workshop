1432a8a9f0f0ddde7ee6b123da63fa1267d6153b
josdem <joseluis.delacruz@gmail.com>
2023-06-03 17:58:12 -0400

    #3 Deleting Groovy kata

--- simple-algorithms/palindrome/PalindromeDetector.groovy
+++
@@ -1,9 +0,0 @@
-/**
-A Palindrome is a word phrase, or number that reads the same backward or forward.
-**/
-
-Boolean isPalindrome(String string){
-  string.reverse().equals(string)
-}
-
-assert isPalindrome('anitalavalatina')
