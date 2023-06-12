4e6ae291131c378e0b16cf312e228b1537e06073
josdem <joseluis.delacruz@gmail.com>
2023-06-12 10:44:07 -0400

    Improving solution

--- simple-algorithms/longest-substring/src/main/java/com/josdem/kata/LongestSubstring.java
+++ simple-algorithms/longest-substring/src/main/java/com/josdem/kata/LongestSubstring.java
@@ -1,7 +1,12 @@
 package com.josdem.kata;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LongestSubstring {
     public int reduce(String data) {
-        return data.length();
+        List<String> substrings =
+                Arrays.stream(data.split("")).distinct().toList();
+        return substrings.size() > 0 ? substrings.size() : 1;
     }
 }
