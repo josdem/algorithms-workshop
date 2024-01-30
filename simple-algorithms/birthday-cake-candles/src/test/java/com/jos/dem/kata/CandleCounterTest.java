e6485b38df634c9067a21e17135fd241a47f8d29
josdem <joseluis.delacruz@gmail.com>
2024-01-30 11:08:11 -0500

    Changing to list.of version

--- simple-algorithms/birthday-cake-candles/src/test/java/com/jos/dem/kata/CandleCounterTest.java
+++ simple-algorithms/birthday-cake-candles/src/test/java/com/jos/dem/kata/CandleCounterTest.java
@@ -8,18 +8,19 @@ Your task is to create a function that removes all small candles and just keep t
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CandleCounterTest {
 
-    private CandleCounter candleCounter = new CandleCounter();
+    private final CandleCounter candleCounter = new CandleCounter();
 
     @Test
     @DisplayName("Keep biggest candles")
     void shouldKeepBiggestCandles() {
-        assertEquals(Arrays.asList(3, 3), candleCounter.count(Arrays.asList(3, 2, 1, 3)));
-        assertEquals(Arrays.asList(1), candleCounter.count(Arrays.asList(1)));
+        assertEquals(List.of(3, 3), candleCounter.count(List.of(3, 2, 1, 3)));
+        assertEquals(List.of(1), candleCounter.count(List.of(1)));
     }
 }
