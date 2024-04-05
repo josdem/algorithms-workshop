e13fc7482b53c3308233122d1471b189a3bccc1d
josdem <joseluis.delacruz@gmail.com>
2024-04-05 10:06:49 -0400

    Adding to list

--- simple-algorithms/longest-substring/src/main/java/com/josdem/kata/LongestSubstring.java
+++ simple-algorithms/longest-substring/src/main/java/com/josdem/kata/LongestSubstring.java
@@ -2,11 +2,12 @@ package com.josdem.kata;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LongestSubstring {
     public int reduce(String data) {
         List<String> substrings =
-                Arrays.stream(data.split("")).distinct().toList();
+                Arrays.stream(data.split("")).distinct().collect(Collectors.toList());
         return !substrings.isEmpty() ? substrings.size() : 1;
     }
 }
