d40abcb0da8e2bab580562c978b057bcca57d406
josdem <joseluis.delacruz@gmail.com>
2024-04-10 14:27:16 -0400

    Updating Gradle and Junit versions

---
+++ simple-algorithms/counting-numbers/src/main/java/com/josdem/kata/NumbersCounter.java
@@ -0,0 +1,11 @@
+package com.josdem.kata;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class NumbersCounter {
+
+    public List<Integer> count(List<Integer> numbers) {
+        return numbers;
+    }
+}
--- simple-algorithms/counting-numbers/src/test/java/com/josdem/kata/NumbersCounterTest.java
+++ simple-algorithms/counting-numbers/src/test/java/com/josdem/kata/NumbersCounterTest.java
@@ -1,4 +1,4 @@
-package com.jos.dem.kata;
+package com.josdem.kata;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
