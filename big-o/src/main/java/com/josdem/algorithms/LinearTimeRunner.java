4f42d770294b16e05f055275be535ef1c88c16e8
josdem <joseluis.delacruz@gmail.com>
2024-04-11 06:12:04 -0400

    Adding linear big-o example

---
+++ big-o/src/main/java/com/josdem/algorithms/LinearTimeRunner.java
@@ -0,0 +1,9 @@
+package com.josdem.algorithms;
+
+import java.util.List;
+
+public class LinearTimeRunner {
+    public List<Integer> getEvenNumbers(List<Integer> numbers) {
+        return numbers;
+    }
+}
