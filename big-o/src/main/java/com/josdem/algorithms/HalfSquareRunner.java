d87b47b8c09d7bafffbbef91bc2ac57b9af2d934
josdem <joseluis.delacruz@gmail.com>
2024-04-19 17:30:44 -0400

    Adding half-square runner example

---
+++ big-o/src/main/java/com/josdem/algorithms/HalfSquareRunner.java
@@ -0,0 +1,9 @@
+package com.josdem.algorithms;
+
+import java.util.List;
+
+public class HalfSquareRunner {
+    public List<Integer> getTargetNumbers(List<Integer> numbers, int target) {
+        return numbers;
+    }
+}
---
+++ big-o/src/test/java/com/josdem/algorithms/HalfSquareRunnerTest.java
@@ -0,0 +1,22 @@
+package com.josdem.algorithms;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HalfSquareRunnerTest {
+
+    private final HalfSquareRunner halfSquareRunner = new HalfSquareRunner();
+
+    @Test
+    @DisplayName("show half-square algorithm")
+    void shouldTestHalfSquare(){
+        List<Integer> numbers = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer> expectedNumbers = List.of(0, 5, 1, 4, 2, 3, 3, 2, 4, 1, 5, 0);
+        int target = 5;
+        assertEquals(expectedNumbers, halfSquareRunner.getTargetNumbers(numbers, target), "should get numbers from the array");
+    }
+}
