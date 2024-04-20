9de1b4c044c99a119c0c8f9f0befc6e5def4f47f
josdem <joseluis.delacruz@gmail.com>
2024-04-20 10:08:28 -0400

    Adding largest prime number runner solution

--- big-o/src/test/java/com/josdem/algorithms/PrimeNumberRunnerTest.java
+++ big-o/src/test/java/com/josdem/algorithms/PrimeNumberRunnerTest.java
@@ -15,7 +15,7 @@ class PrimeNumberRunnerTest {
     @DisplayName("show array plus compute logic algorithm")
     void shouldReturnLargestPrimeNumber(){
         List<Integer> numbers = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-        int expectedNumber = 7;
-        assertEquals(expectedNumber, primeNumberRunner.getNumber(numbers), "should get largest prime number in the array");
+        List<Integer> expectedNumbers = List.of(2, 3, 5, 7);
+        assertEquals(expectedNumbers, primeNumberRunner.getNumber(numbers), "should get largest prime number in the array");
     }
 }
