e0e703a003e62a48a50ce64143ee112e858727b8
josdem <joseluis.delacruz@gmail.com>
2024-05-07 10:27:01 -0400

    Fixing compilation error

--- simple-algorithms/create-phone-number/src/test/java/com/jos/dem/kata/PhoneNumberGeneratorTest.java
+++ simple-algorithms/create-phone-number/src/test/java/com/jos/dem/kata/PhoneNumberGeneratorTest.java
@@ -15,6 +15,6 @@ class PhoneNumberGeneratorTest {
   @DisplayName("Format a phone number")
   void shouldFormatPhoneNumber() {
     assertEquals(
-        "(123) 456-7890", phoneNumberGenerator.format(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0));
+        "(123) 456-7890", phoneNumberGenerator.format(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)));
   }
 }
