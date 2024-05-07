02c481d1562749872e627f7d43fd3a25046a04d1
josdem <joseluis.delacruz@gmail.com>
2024-05-07 12:41:43 -0400

    Improving solution

--- simple-algorithms/smallest-biggest/src/main/java/com/josdem/kata/SmallerBiggestFinder.java
+++ simple-algorithms/smallest-biggest/src/main/java/com/josdem/kata/SmallerBiggestFinder.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class SmallerBiggestFinder {
     public List<Integer> find(List<Integer> numbers) {
-        Integer smaller = numbers.stream().reduce((a, b) -> a < b ? a : b).get();
-        Integer biggest = numbers.stream().reduce((a, b) -> a > b ? a : b).get();
+        Integer smaller = numbers.stream().min(Integer::compare).orElse(0);
+        Integer biggest = numbers.stream().max(Integer::compare).orElse(0);
         return Arrays.asList(smaller, biggest);
     }
 }
