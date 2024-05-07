f76548809edee53d4f11534658fd6bd972846188
josdem <joseluis.delacruz@gmail.com>
2024-05-07 10:25:52 -0400

    Deleting solution

--- simple-algorithms/create-phone-number/src/main/java/com/jos/dem/kata/PhoneNumberGenerator.java
+++ simple-algorithms/create-phone-number/src/main/java/com/jos/dem/kata/PhoneNumberGenerator.java
@@ -4,10 +4,6 @@ import java.util.List;
 
 public class PhoneNumberGenerator {
     public String format(List<Integer> numbers) {
-        final StringBuilder sb = new StringBuilder();
-        String arrayAsString =
-                numbers.stream().map(String::valueOf).reduce("", String::concat);
-        sb.append(arrayAsString).insert(0, '(').insert(4, ')').insert(5, ' ').insert(9, '-');
-        return sb.toString();
+        return numbers.toString();
     }
 }
