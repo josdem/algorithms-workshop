1e5ab20176a96428b1fe2bc2bbfa36b319f3e69d
josdem <joseluis.delacruz@gmail.com>
2024-05-07 12:18:48 -0400

    Adding solution

--- simple-algorithms/create-phone-number/src/main/java/com/josdem/kata/PhoneNumberGenerator.java
+++ simple-algorithms/create-phone-number/src/main/java/com/josdem/kata/PhoneNumberGenerator.java
@@ -1,9 +1,13 @@
 package com.josdem.kata;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PhoneNumberGenerator {
     public String format(List<Integer> numbers) {
-        return numbers.toString();
+        StringBuilder sb = new StringBuilder();
+        String numbersAsString = numbers.stream().map(String::valueOf).collect(Collectors.joining(""));
+        sb.append(numbersAsString).insert(0, '(').insert(4, ')').insert(5, ' ').insert(9, '-');
+        return sb.toString();
     }
 }
